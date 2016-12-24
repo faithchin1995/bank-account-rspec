7317bdf089da5cd2e1ff7bb0ff3b6bfe9ec28c6f
Faith <chinlin58@gmail.com>
2016-12-25 02:37:26 +0800

    bank account

--- account_spec.rb
+++ account_spec.rb
@@ -1,29 +1,54 @@
 require "rspec"
-
 require_relative "account"
 
-describe Account do
-  describe "#initialize" do
 
+describe Account do
+ 
+ let(:acct_num) {Account.new("1234567890")}
+ 
+  describe "#initialize" do
+    it "returns an account object" do
+      x = Account.new("0123456789")
+      expect(x.class).to eq(Account)
+    end
   end
 
   describe "#transactions" do
-
+    it "gives specified starting balance" do
+      expect(acct_num.transactions[0]).to eq(0)
+    end
   end
 
   describe "#balance" do
+    it "gives back balance" do
 
+      y = Account.new("2345678910", 100)
+      y.deposit!(100)
+      expect(y.balance).to be(200)
+    end
   end
 
   describe "#account_number" do
-
+    it "shows the last 4 digits" do
+      expect(acct_num.acct_number).to eq("******7890")
+    end
   end
 
   describe "deposit!" do
+    it "shows the balance after deposit" do
 
+      z = Account.new("3456789012", 500)
+
+      expect(z.deposit!(500)).to be(1000)
+    end
   end
 
   describe "#withdraw!" do
+    it "shows the balance after withdrawal" do
 
+      y = Account.new("4567890123", 1000)
+
+      expect(y.withdraw!(200)).to be(800)
+    end
   end
 end
